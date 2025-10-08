7db805140c91a9ea1be6299a19987fd3f8a7f72e
Shubham Dutta <76271187+shubhamdutta26@users.noreply.github.com>
2025-10-08 11:26:25 -0400

    Create install_Rpkgs.R

---
+++ install_Rpkgs.R
@@ -0,0 +1,30 @@
+#!/usr/bin/env Rscript
+
+start_time <- proc.time()
+
+options(
+  repos = c(CRAN = "https://packagemanager.posit.co/cran/latest"),
+  pak.upgrade = TRUE
+)
+
+install.packages('pak')
+pak::pkg_install(c(
+  'ragg',
+  'tidyverse',
+  'gt',
+  'ggtext',
+  'ggsci',
+  'here',
+  'devtools'
+))
+
+end_time <- proc.time()
+elapsed <- end_time - start_time
+cat(
+  sprintf(
+    "Elapsed time: %02d:%02d:%02.1f (h:m:s)\n",
+    as.integer(elapsed[3] %/% 3600),
+    as.integer((elapsed[3] %% 3600) %/% 60),
+    elapsed[3] %% 60
+  )
+)
