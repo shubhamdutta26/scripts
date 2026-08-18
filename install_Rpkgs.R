07d8f9a98d82a44eebfcbc149b78b2dd5f340e46
Shubham Dutta <76271187+shubhamdutta26@users.noreply.github.com>
2026-08-17 21:00:52 -0400

    Add V8 package installation for Arch Linux
    Added installation of V8 package for Arch Linux.

--- install_Rpkgs.R
+++ install_Rpkgs.R
@@ -7,6 +7,11 @@ options(
   pak.upgrade = TRUE
 )
 
+# For arch linux ----
+Sys.setenv(DOWNLOAD_STATIC_LIBV8 = "1")
+install.packages("V8")
+# -----
+
 install.packages('pak')
 pak::pkg_install(c(
   'ragg',
